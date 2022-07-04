0a527908fbc3f36dd67e5cbb62876f988ac0d445
Mateusz <mateusz97bieniek@gmail.com>
2022-07-04 16:59:13 +0100

    additional changes by Emma

--- enemy_grid.py
+++ enemy_grid.py
@@ -1,9 +1,10 @@
-temp_list = ["|"]*5
-grid_size =5
+# ▢ - water ▣ - hit water
+# ☆ - ship  ★ - hit ship
 
-
-for i in range(grid_size):
-  temp_list.append(["|"] *5)
-
-
-print(temp_list)
+enemygrid = {
+  "A1": "▢", "A2": "▢", "A3": "▢", "A4": "▢", "A5": "▢",
+  "B1": "▢", "B2": "▢", "B3": "▢", "B4": "▢", "B5": "▢",
+  "C1": "▢", "C2": "▢", "C3": "▢", "C4": "▢", "C5": "▢",
+  "D1": "▢", "D2": "▢", "D3": "▢", "D4": "▢", "D5": "▢",
+  "E1": "▢", "E2": "▢", "E3": "▢", "E4": "▢", "E5": "▢"
+}
